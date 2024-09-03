4f47ff54dc4fa91b74d46e49750668cab1ca05fe
Jumana Iqbal <jumanamohamediqbal@gmail.com>
2024-09-03 13:51:37 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TicTacToe from './Components/TicTacToe/TicTacToe';
+
+function App() {
+	return (
+		<div>
+			<TicTacToe />
+		</div>
+	);
+}
+
+export default App;
